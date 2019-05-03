9ebf3d0201e00f56e10fc3bfa85b60f3eaf2644f
kate <ket2.nguyen.huu@gmail.com>
2019-05-03 21:53:05 +0700

    + Add categories for returning of register api and login api

--- app/Model/SNSInfo.php
+++ app/Model/SNSInfo.php
@@ -10,7 +10,7 @@ class SNSInfo extends Eloquent
     protected $connection = 'mongodb';
     protected $collection = 'statistic';
     protected $hidden = ['_id', 'exception'];
-    protected $cast = [
+    protected $casts = [
         'social_type' => 'string'
     ];
 }
