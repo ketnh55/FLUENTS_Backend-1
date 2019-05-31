89528311e11df5a7b9e97fbdbbcaec5af70a8cd4
KetNH <nguyen.huu.ket@alliedtechbase.com>
2019-05-31 16:57:44 +0700

    update link_sns api

--- resources/lang/en/response_message.php
+++ resources/lang/en/response_message.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'username_or_password_wrong' => 'Username or Password is wrong',
+    'username_or_password_wrong' => 'Username or password is incorrect',
     'user_not_active' => 'user is not active',
     'status_success' => 'success'
 ];
